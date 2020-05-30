a7da06004c000c954528de5970872fd0d19a30b8
Lusca Lima <lucas@siasp.com.br>
2020-05-30 12:26:11 -0300

    File App.js cleaned

--- src/App.js
+++ src/App.js
@@ -2,17 +2,7 @@ import React from "react";
 import Section from "./components/Section";
 
 function App() {
-  return (
-    <div className="App">
-      <Section>
-        <h1>Olá Mundo Carcará Hackspace</h1>
-      </Section>
-
-      <Section style={{ backgroundColor: "#f2f2f2" }}>
-        <h1>Aqui vem o sobre</h1>
-      </Section>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
